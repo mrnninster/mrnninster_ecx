f1b8c5ce4d81f5a1804e10c99314ad7a82a78f1c
Akinsola <41529409+mrnninster@users.noreply.github.com>
2020-03-27 17:29:34 -0700

    function

--- Adefolahan_akinsola_day0.py
+++ Adefolahan_akinsola_day0.py
@@ -1,13 +1,13 @@
-word=input('Enter a word ')
-L=len(word)
-sm=0
-alphabets={"a":1,"b":3,"c":3,"d":2,"e":1,"f":4,"g":2,
-          "h":4,"i":1,"j":8,"k":5,"l":1,"m":3,"n":1,
-          "o":1,"p":3,"q":10,"r":1,"s":1,"t":1,"u":1,
-          "v":4,"w":4,"x":8,"y":4,"z":10}
-for w in range(0,L):
-
-    letter=word[w]
-    nv=alphabets[letter]
-    sm=sm+nv
-print(sm)
+def scrabble():
+    word=input('Enter a word ')
+    L=len(word)
+    sm=0
+    alphabets={"a":1,"b":3,"c":3,"d":2,"e":1,"f":4,"g":2,
+              "h":4,"i":1,"j":8,"k":5,"l":1,"m":3,"n":1,
+              "o":1,"p":3,"q":10,"r":1,"s":1,"t":1,"u":1,
+              "v":4,"w":4,"x":8,"y":4,"z":10}
+    for w in range(0,L):
+        letter=word[w]
+        nv=alphabets[letter]
+        sm=sm+nv
+    print(sm)
