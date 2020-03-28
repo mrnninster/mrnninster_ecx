4f9e2c1b00dd703c2ccb82068069bbb5c0c4ceac
Akinsola <41529409+mrnninster@users.noreply.github.com>
2020-03-27 22:05:37 -0700

    password generator

---
+++ Adefolahan_Akinsola_day2.py
@@ -0,0 +1,14 @@
+import random, string
+val=input('Define your password length ')
+val=int(val)
+
+def password_generator(val):
+    #generating Alphanumeric string
+    alphaNum=string.ascii_letters + string.digits
+    password=""
+    #Password Constructor
+    for x in range(0,val):
+        randalpha = random.randrange(0,len(alphaNum))
+        password += alphaNum[randalpha]
+    print(password)
+password_generator(val)
