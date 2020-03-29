88c2b7d455460ff3615cdbc28121dd37c1f153f8
Akinsola <41529409+mrnninster@users.noreply.github.com>
2020-03-29 03:50:43 -0700

    Fixed Security issues

--- Adefolahan_akinsola_day4.py
+++ Adefolahan_akinsola_day4.py
@@ -1,11 +1,36 @@
-cell=input('Enter Phone number')
-#Number check funtion
+#String Input for number with or without country code
+cell=input('Enter Phone number ')
+
+#Function to check the country
 def is_Nigerian(cell):
-    N=['081','080','090','070','234']
     cc=len(cell)
     if(cc!=12) and (cc in range(11,15)):
-        for n in range(0,5):
-            checker=N[n] in cell
+        #for numbers starting with 0** and 234
+        if(cc==11) or (cc==13):
+            N=['081','080','090','070','234']
+            ccs=cell[:3]
+            for n in range(0,5):
+                checker=N[n] in ccs
+                if checker==True:
+                    print(str(cell)+' is Nigerian')
+                
+                #When the number id not nigerian
+                else:
+                    print(str(cell)+' is not Nigerian')
+                    break
+        #for numbers starting with +234
+        if(cc==14):
+            ccs=cell[:4]
+            checker='+234' in ccs
             if checker==True:
                 print(str(cell)+' is Nigerian')
+                
+            #When the number id not nigerian
+            else:
+                print(str(cell)+' is not Nigerian')
+        
+    #When the number id not nigerian
+    else:
+        print(str(cell)+' is not Nigerian')
+                
 is_Nigerian(cell)
